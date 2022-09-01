2d90c3674c61acb11a7708f4efb983de5a5b9e6c
hugotseixas <seixas.hugo@protonmail.com>
2022-09-01 14:51:54 -0300

    Rename files and add more variables to the model.

--- iclfs_projections/04_run_model.R
+++ iclfs_projections/04_run_model.R
@@ -6,7 +6,7 @@
 #
 #
 # Authors:      Hugo Tameirao Seixas
-# Contact:      tameirao.hugo@gmail.com
+# Contact:      seixas.hugo@protonmail.com
 # Date:         2022-07-07
 #
 # Notes:
@@ -26,7 +26,7 @@ library(tidyverse)
 # LOAD BASE GRID --------------------------------------------------------------
 
 ## Load raster file
-r <- rast("iclfs_projections/base_grid_pasture.tif")
+r <- rast("iclfs_projections/data/base_grid_pasture.tif")
 
 # GENERATE RANDOM DATA AS DUMMIES ---------------------------------------------
 
@@ -74,8 +74,8 @@ transition_length <-
 tree_pop <-
   reduce(
     map2(
-      .x = c(333, 476, 370, 690, 715),
-      .y = sample(1:40, 5, replace = TRUE),
+      .x = c(333, 400, 476, 500, 370, 690, 715, 900, 1292),
+      .y = sample(1:40, 9, replace = TRUE),
       .f = ~ { rep(.x, .y) }),
     c
   )
@@ -122,11 +122,11 @@ lulc <- as_tibble(as.data.frame(r, na.rm = FALSE, cells = TRUE)) %>%
 number_of_rows <- nrow(lulc)
 
 ## Calculate a linear expansion rate
-annual_increase <- floor(pull(global(r, sum, na.rm = TRUE) / 30))
+annual_increase <- floor(pull(global(r, sum, na.rm = TRUE) / 70))
 
 # START LAND COVER PROJECTIONS ------------------------------------------------
 
-for (y in 2021:2050) {
+for (y in 2021:2100) {
 
   sample_pool <- lulc %>%
     filter(
@@ -141,31 +141,31 @@ for (y in 2021:2050) {
     mutate(area_sum = cumsum(area)) %>%
     filter(area_sum < annual_increase + 10)
 
-  new_system <- sample(systems, size = number_of_rows, replace = TRUE)
+  sample_system <- sample(systems, size = number_of_rows, replace = TRUE)
 
-  new_cycle <- sample(cycle, size = number_of_rows, replace = TRUE)
+  sample_cycle <- sample(cycle, size = number_of_rows, replace = TRUE)
 
-  new_transition <- sample(transition_systems, size = number_of_rows, replace = TRUE)
+  sample_transition <- sample(transition_systems, size = number_of_rows, replace = TRUE)
 
-  new_transition_length <- sample(transition_length, size = number_of_rows, replace = TRUE)
+  sample_transition_length <- sample(transition_length, size = number_of_rows, replace = TRUE)
 
-  new_tree_pop <- sample(tree_pop, size = number_of_rows, replace = TRUE)
+  sample_tree_pop <- sample(tree_pop, size = number_of_rows, replace = TRUE)
 
-  new_system_management <- sample(system_management, size = number_of_rows, replace = TRUE)
+  sample_system_management <- sample(system_management, size = number_of_rows, replace = TRUE)
 
-  new_transition_management <- sample(transition_management, size = number_of_rows, replace = TRUE)
+  sample_transition_management <- sample(transition_management, size = number_of_rows, replace = TRUE)
 
   new_year <- lulc %>%
     filter(year == y - 1) %>%
     mutate(
       year = y,
-      new_cover = new_system,
-      new_cycle = new_cycle,
-      new_transition = new_transition,
-      mew_transition_length = new_transition_length,
-      new_tree_pop = new_tree_pop,
-      new_system_management = new_system_management,
-      new_transition_management = new_transition_management,
+      new_cover = sample_system,
+      new_cycle = sample_cycle,
+      new_transition = sample_transition,
+      new_transition_length = sample_transition_length,
+      new_tree_pop = sample_tree_pop,
+      new_system_management = sample_system_management,
+      new_transition_management = sample_transition_management,
       transition = if_else(cell %in% sample_sub$cell, new_transition, transition),
       transition_length = if_else(cell %in% sample_sub$cell, new_transition_length, transition_length),
       cover = if_else(cell %in% sample_sub$cell, new_system, cover),
@@ -179,13 +179,23 @@ for (y in 2021:2050) {
   new_year <- new_year %>%
     mutate(
       cover = if_else(cycle_end - year == 0, new_system, cover, missing = cover),
+      tree_pop = if_else(cycle_end - year == 0, new_tree_pop, tree_pop, missing = tree_pop),
+      system_management = if_else(cycle_end - year == 0, new_system_management, system_management, missing = system_management),
+      transition_management = if_else(cycle_end - year == 0, new_transition_management, transition_management, missing = transition_management),
       transition = if_else(cycle_end - year == 0, new_transition, transition, missing = transition),
       transition_length = if_else(cycle_end - year == 0, new_transition_length, transition_length, missing = transition_length),
       cycle_lenght = if_else(cycle_end - year == 0, new_cycle, cycle_lenght, missing = cycle_lenght),
       cycle_end = if_else(cycle_end - year == 0, year + cycle_lenght + transition_length, cycle_end, missing = cycle_end)
     ) %>%
-    select(cell:transition_length)
+    select(
+      cell, area, year, cover, transition, transition_length, cycle_lenght,
+      cycle_end, tree_pop, system_management, transition_management
+    )
 
   lulc <- bind_rows(lulc, new_year)
 
 }
+
+lulc %>%
+  filter(cell == sample(lulc$cell, size = 1)) %>%
+  view()
