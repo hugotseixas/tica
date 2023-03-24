af53703fce1887f18db0e0fbfb327c1d61a3011c
hugotseixas <seixas.hugo@protonmail.com>
2023-03-24 17:02:16 -0300

    Add function to create base grid for the model.

--- R/create_grid.R
+++ R/create_grid.R
@@ -1,31 +1,41 @@
+#' Create base grid for TICA
+#'
+#' @param resolution Set the resolution of the grid in degrees.
+#' @param full_cells Choose if cells should completely inside the biomes.
+#'
+#' @return A tibble.
+#' @export
+#'
+#' @examples
+#' base_grid <- create_grid(resolution = 0.2, full_cells = TRUE)
 create_grid <- function(resolution, full_cells) {
 
   # Load biomes limit
   biomes <-
-    read_biomes(
+    geobr::read_biomes(
       year = 2019,
       simplified = TRUE, # Biome limits are simplified (topology preserved)
       showProgress = FALSE
-    ) %>%
-    filter(name_biome %in% c("Amazônia", "Cerrado"))
+    ) |>
+    dplyr::filter(code_biome %in% c(1, 3))
 
   # Create grid based on polygons
   grid <-
-    st_make_grid(
+    sf::st_make_grid(
       x = biomes,
       cellsize = resolution # Resolution of the grid cell
-    ) %>%
-    st_as_sf() %>%
-    st_join(
+    ) |>
+    sf::st_as_sf() |>
+    sf::st_join(
       y = biomes,
       left = FALSE,
       # Choose filter function based on option above
-      join = if (full_cells) {st_within} else {st_intersects}
-    ) %>%
-    select(name_biome, geometry) %>%
-    mutate(
-      cell_id = row_number(),
-      cell_area = st_area(geometry)
+      join = if (full_cells) { sf::st_within } else { sf::st_intersects }
+    ) |>
+    dplyr::select(name_biome, geometry) |>
+    dplyr::mutate(
+      cell_id = dplyr::row_number(),
+      cell_area = sf::st_area(geometry)
     )
 
   return(grid)
